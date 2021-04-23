ecdf5a1c5f429fd9e3b90eb43a8f81013c1a78b5
ShadoWADC <walkeralonso500net@gmail.com>
2021-04-23 21:20:34 +0200

    Stable Version - Tested with 3 Users
    This is a stable first version, I have tested it for 3 users. I need to check furthermore the code with Valgrind.
    Need to clean the code of comments and debug printfs.

--- client.c
+++ client.c
@@ -6,10 +6,46 @@
 #include <arpa/inet.h>  // htons() and inet_addr()
 #include <netinet/in.h> // struct sockaddr_in
 #include <sys/socket.h>
-
+#include <time.h>
+ 
 #include "common.h"
 
+struct login                           
+{
+    char username[30];
+    char password[20];
+};
+struct login l;
+int login (void);
+void registration (void);
+
 int main(int argc, char* argv[]) {
+	int option;
+	do{
+		printf("Chat Service Menu:");
+		printf("\n\t(1)Register\n\t(2)Login\n\t(0)Exit\n\n");
+		printf("Select your option: ");
+		scanf("%d",&option);
+		while( getchar() != '\n' ){ /* flush to end of input line */ }
+
+		
+		if(option == 1)
+		{
+			system("clear");
+			registration();
+		}
+
+		else if(option == 2)
+			{
+				system("clear");
+				if(login() == 0)break;
+			}
+		else if (option == 0){
+				exit(EXIT_SUCCESS);
+		}	
+	}while (option != 0);
+	
+	
     int ret,bytes_sent,recv_bytes;
 
     // variables for handling a socket
@@ -17,7 +53,8 @@ int main(int argc, char* argv[]) {
     struct sockaddr_in server_addr = {0}; // some fields are required to be filled with 0
 
     // create a socket
-    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    //socket_desc = socket(AF_INET, SOCK_DGRAM, 0); 
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0); 
     if(socket_desc < 0) handle_error("Could not create socket");
 
     // set up parameters for the connection
@@ -35,8 +72,28 @@ int main(int argc, char* argv[]) {
     size_t buf_len = sizeof(buf);
     int msg_len;
     memset(buf, 0, buf_len);
-
-    // display welcome message from server
+	
+	//send username to server
+	bytes_sent=0;
+	ret = 0;
+	
+	memcpy(buf, l.username, buf_len); //Copying message into buffer
+	msg_len = strlen(buf)+1;
+    buf[strlen(buf)] = '\n'; // remove '\n' from the end of the message 
+    printf("\n\nUsername selected <%s>\n\n",buf);
+    
+	while ( bytes_sent < msg_len) {
+		ret = send(socket_desc, buf + bytes_sent, msg_len - bytes_sent, 0);
+		if (ret == -1 && errno == EINTR) continue;
+		if (ret == -1) handle_error("Cannot write to the socket");
+		bytes_sent += ret;
+	}
+    
+      
+        
+   // display welcome message from server
+    memset(buf, 0, buf_len);
+    buf_len = sizeof(buf);
     recv_bytes = 0;
     do {
         ret = recv(socket_desc, buf + recv_bytes, buf_len - recv_bytes, 0);
@@ -51,44 +108,153 @@ int main(int argc, char* argv[]) {
     while (1) {
         char* quit_command = SERVER_COMMAND;
         size_t quit_command_len = strlen(quit_command);
+        do{
+			system("clear");
+			printf("Welcome to the Chat Client!\nPlease select an option:\n");
+			printf("\t(1) Send a message\n");
+			printf("\t(2) Check recieved messages\n");
+			printf("\t(0) Exit\nSelect your option: ");
+			scanf("%d",&option);
+			while( getchar() != '\n' ){ /* flush to end of input line */ }
+		}while(option<0||option>2);
+		
+		
+		if (option == 0){
+				exit(EXIT_SUCCESS);
+		}
+		else if(option == 1){ //SEND
+			system("clear");
+			printf("MESSAGE FORMAT = USERNAME_DEST:MESSAGE\n");
+			printf("Insert your message: ");
 
-        printf("Insert your message: ");
+			/* Read a line from stdin
+			 *
+			 * fgets() reads up to sizeof(buf)-1 bytes and on success
+			 * returns the first argument passed to it. */
+			if (fgets(buf, sizeof(buf), stdin) != (char*)buf) {
+				fprintf(stderr, "Error while reading from stdin, exiting...\n");
+				exit(EXIT_FAILURE);
+			}
+			
+			//MESSAGE: USER_SOURCE:USER_DESTINATION:MESSAGE
+			//Concatenating USER to the message Header
+			char dest[1024];
+			strcpy( dest, l.username );
+			strcat( dest, ":");
+			strcat( dest, buf);
+			memcpy(buf, dest, sizeof(buf));
+			printf("\nBuffer %s",buf);
+			printf("\nStrlen:%ld\nSizeof:%zu\n",strlen(buf),sizeof(buf));
+			
+			
+			msg_len = strlen(buf);
+			buf[strlen(buf) - 1] = '\n'; // remove '\n' from the end of the message
+			// send message to server
+			bytes_sent=0;
+			while ( bytes_sent < msg_len) {
+				ret = send(socket_desc, buf + bytes_sent, msg_len - bytes_sent, 0);
+				if (ret == -1 && errno == EINTR) continue;
+				if (ret == -1) handle_error("Cannot write to the socket");
+				bytes_sent += ret;
+			}
 
-        /* Read a line from stdin
-         *
-         * fgets() reads up to sizeof(buf)-1 bytes and on success
-         * returns the first argument passed to it. */
-        if (fgets(buf, sizeof(buf), stdin) != (char*)buf) {
-            fprintf(stderr, "Error while reading from stdin, exiting...\n");
-            exit(EXIT_FAILURE);
-        }
+			/* After a quit command we won't receive any more data from
+			 * the server, thus we must exit the main loop. */
+			if (msg_len == quit_command_len && !memcmp(buf, quit_command, quit_command_len)) break;
+		}
+		else if(option == 2){ //RECIEVE
+			
+			// Calculate the time taken by fun()
+			/*clock_t start,current;
+			start = clock();*/
+  
+			// read message from server
+			recv_bytes = 0;
+			//int timeout_flag = -1;
+			do {
+				ret = recv(socket_desc, buf + recv_bytes, buf_len - recv_bytes, 0);
+				if (ret == -1 && errno == EINTR) continue;
+				if (ret == -1) handle_error("Cannot read from the socket");
+				if (ret == 0) break;
+				recv_bytes += ret;
+				
+				/*current = clock() - start;
+				double time_taken = ((double)current)/CLOCKS_PER_SEC; // in seconds
+				if(TIMEOUT < time_taken){
+					printf("Server timeout: No messages in your inbox\n"); 	
+					timeout_flag = 1;
+					break;
+				}*/
+			} while ( buf[recv_bytes-1] != '\n' );
+			
+			/*if(!timeout_flag)*/ printf("Server response: %s\n", buf); // no need to insert '\0'
+			
+			//--------------
+			//GET SENDER - USERNAME
+			int len = strlen(buf);
+			int semi = 0;
+			int pos1 = 0, pos2 = 0;
+			//pos1 pos2 SENDER
+			//From pos3 to l is MSG
 
-        msg_len = strlen(buf);
-        buf[strlen(buf) - 1] = '\n'; // remove '\n' from the end of the message
-        // send message to server
-        bytes_sent=0;
-        while ( bytes_sent < msg_len) {
-            ret = send(socket_desc, buf + bytes_sent, msg_len - bytes_sent, 0);
-            if (ret == -1 && errno == EINTR) continue;
-            if (ret == -1) handle_error("Cannot write to the socket");
-            bytes_sent += ret;
-        }
+	
+	
+			int start = 0; //Start of Message
+			for(int i = 0; i<len; i++){
+				if(buf[i]==':') {
+					semi++;
+					if(semi == 1){
+						pos2 = i;
+						
+					}
+				}	
+			}
+			
+			const int size_dest = pos2-pos1+1;
+			char sender[size_dest];
+			int k = 0;
+			for(int i = pos1; i<pos2; i++){
+				sender[k] = buf[i];
+				k++;
+			}
+			sender[size_dest-1] = '\0';
+			
 
-        /* After a quit command we won't receive any more data from
-         * the server, thus we must exit the main loop. */
-        if (msg_len == quit_command_len && !memcmp(buf, quit_command, quit_command_len)) break;
+			//Ricostruire messaggio
+			//SENDER:ME:MSG
+			
+			//MESSAGE: USER_SOURCE:USER_DESTINATION:MESSAGE
+			//Concatenating USER to the message Header
+			if(semi>=2){
+				char temp[1024];
+				memcpy(temp, l.username, sizeof(temp));
+				strcat( temp, ":"); 
+				strcat( temp, sender);
 
-        // read message from server
-	    recv_bytes = 0;
-    	do {
-            ret = recv(socket_desc, buf + recv_bytes, buf_len - recv_bytes, 0);
-            if (ret == -1 && errno == EINTR) continue;
-            if (ret == -1) handle_error("Cannot read from the socket");
-	        if (ret == 0) break;
-	        recv_bytes += ret;
+				strcat( temp, ":"); 
+				strcat( temp, "Message Read\n"); 
+				memcpy(buf, temp, sizeof(buf));
+				
+				
+				msg_len = strlen(buf);
+				// send message to server
+				bytes_sent=0;
+				while ( bytes_sent < msg_len) {
+					ret = send(socket_desc, buf + bytes_sent, msg_len - bytes_sent, 0);
+					if (ret == -1 && errno == EINTR) continue;
+					if (ret == -1) handle_error("Cannot write to the socket");
+					bytes_sent += ret;
+				}
+			}
 
-	    } while ( buf[recv_bytes-1] != '\n' );
-        printf("Server response: %s\n", buf); // no need to insert '\0'
+			//---------
+			
+			printf("Press any key to go back to the main MENU...");
+			getchar();	
+		}
+        
+
+        
     }
 
 
@@ -99,4 +265,78 @@ int main(int argc, char* argv[]) {
     if (DEBUG) fprintf(stderr, "Exiting...\n");
 
     exit(EXIT_SUCCESS);
+}
+
+int login (void)
+{
+    char username[30],password[20];
+	int match = -1;
+    FILE *log;
+
+    log = fopen("login.txt","r");
+    if (log == NULL)
+    {
+        fputs("Error at opening File!", stderr);
+        exit(1);
+    }
+
+    printf("\nPlease Enter your login credentials below\n\n");
+    printf("Username:  ");
+    scanf("%s",username);
+    while( getchar() != '\n' ){ /* flush to end of input line */ }
+    printf("\nPassword: ");
+    scanf("%s",password);
+    while( getchar() != '\n' ){ /* flush to end of input line */ }
+	
+    while(fread(&l,sizeof(l),1,log))
+        {
+        if(strcmp(username,l.username)==0 && strcmp(password,l.password)==0)
+            {   
+                printf("\nLogin Successful!\n");
+                match = 0;
+                break;
+            }
+        else 
+            {
+                printf("\nIncorrect credentials!\nPlease enter the correct credentials\n");
+            }
+        }
+
+    fclose(log);
+    return match;
+}
+
+
+void registration(void)
+{
+    FILE *log;
+
+    log=fopen("login.txt","a");
+    if (log == NULL)
+    {
+        fputs("Error at opening File!", stderr);
+        exit(1);
+    }
+
+    struct login l;
+
+    printf("\nWelcome to the chat service, please proceed with the registration of your account.\n\n");
+
+    printf("\nEnter Username:\n");
+    scanf("%s",l.username);
+    while( getchar() != '\n' ){ /* flush to end of input line */ }
+    printf("\nEnter Password:\n");
+    scanf("%s",l.password);
+    while( getchar() != '\n' ){ /* flush to end of input line */ }
+
+
+    fwrite(&l,sizeof(l),1,log);
+    fclose(log);
+	
+	printf("\nRegistration Successful!\n");
+    printf("\nWelcome, %s!\n\n",l.username);
+
+    printf("Press any key to continue...");
+    getchar();
+    system("clear");
 }
--- server.c
+++ server.c
@@ -7,8 +7,19 @@
 #include <arpa/inet.h>  // htons()
 #include <netinet/in.h> // struct sockaddr_in
 #include <sys/socket.h>
+#include <semaphore.h>
 #include "common.h"
 #include <pthread.h>
+
+/*
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <semaphore.h>
+
+
+#define CRITICAL_SECTION "/critical_section"
+sem_t *critical_section = NULL;
+*/
 
 typedef struct handler_args_s
 {
@@ -21,10 +32,104 @@ typedef struct handler_args_s
     struct sockaddr_in* client_addr;
 } handler_args_t;
 
+struct users_online_t                           
+{
+	int socket_desc[MAX_USERS]; //SOCKET DESCRIPTORS
+    char list[MAX_USERS][30]; //USERNAME
+    int online; //NUMBER OF USERS ONLINE
+    char client_ip[MAX_USERS][INET_ADDRSTRLEN]; //IP ADDRESS 
+	uint16_t client_port[MAX_USERS];	//PORT
+};
+struct users_online_t users;
+
+void printOnlineUsers(int id){
+	system("clear");
+	printf("Welcome to the Chat Service 1.0\n");
+	printf("Online Users List(%d/%d)\n", users.online, MAX_USERS);
+	
+	for(int i = 0; i<users.online ; i++){
+		printf("ID:%d\tUsername:%s\tClient:%s\tPort:%hu\tDescriptor: %d\n", i, users.list[i], users.client_ip[i], users.client_port[i], users.socket_desc[i]);	
+	}
+}
+
+int getSenderDescriptor(char * msg){
+	int l = strlen(msg);
+	
+	//Check Format
+	int semi = 0;
+	int pos1 = 0, pos2 = 0; //Pos of Destinatario 
+	int start = 0; //Start of Message
+	for(int i = 0; i<l; i++){
+		if(msg[i]==':') {
+			semi++;
+			if(semi == 1){
+				pos2 = i;
+				break;
+			}
+		}	
+	}
+		
+	const int size_dest = pos2-pos1/*+1*/;
+	char sender [size_dest];
+	int k = 0;
+	//printf("\nDESTINATARIO: ");
+	for(int i = pos1; i<pos2; i++){
+		sender[k] = msg[i];
+		//printf("%c", destinatario[k]);
+		k++;
+	}
+	
+	for(int i = 0; i<users.online; i++){
+		if(!memcmp(users.list[i], sender, sizeof(sender))) return users.socket_desc[i];
+	}
+	
+	return -3; //USER NOT ONLINE
+}
+
+int isDeliverable(char * msg){
+	int l = strlen(msg);
+	if(l<7) return -1; //Test lunghezza minima package
+	
+	//Check Format
+	int semi = 0;
+	int pos1 = 0, pos2 = 0; //Pos of Destinatario 
+	int start = 0; //Start of Message
+	for(int i = 0; i<l; i++){
+		if(msg[i]==':') {
+			semi++;
+			if(semi == 1)pos1 = i+1;
+			if(semi==2){ 
+				start = i+1;
+				pos2 = i;
+			}
+		}	
+	}
+	if(semi<2) return -2;
+	
+
+	
+	const int size_dest = pos2-pos1;
+	char destinatario [size_dest];
+	int k = 0;
+	for(int i = pos1; i<pos2; i++){
+		destinatario[k] = msg[i];
+		k++;
+	}
+
+	//Search in connected USERS:
+	if (users.online==1) return -3; //Online da solo -> USER NOT ONLINE
+	
+	for(int i = 0; i<users.online; i++){
+		if(!memcmp(users.list[i], destinatario, sizeof(destinatario))) return users.socket_desc[i];
+	}
+	
+	return -3; //USER NOT ONLINE
+}
 
 void connection_handler(int socket_desc, struct sockaddr_in* client_addr) {
     int ret, recv_bytes;
-
+	int id;
+	
     char buf[1024];
     size_t buf_len = sizeof(buf);
     int msg_len;
@@ -36,8 +141,50 @@ void connection_handler(int socket_desc, struct sockaddr_in* client_addr) {
     char client_ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &(client_addr->sin_addr), client_ip, INET_ADDRSTRLEN);
     uint16_t client_port = ntohs(client_addr->sin_port); // port number is an unsigned short
-
-    // send welcome message
+	
+	//recieve username
+	// read message from client
+	memset(buf, 0, buf_len);
+	recv_bytes = 0;
+	do {
+		ret = recv(socket_desc, buf + recv_bytes, 1, 0);
+		if (ret == -1 && errno == EINTR) continue;
+		if (ret == -1) handle_error("Cannot read from the socket");
+		if (ret == 0) break;
+	} while ( buf[recv_bytes++] != '\n' );
+	
+	buf[strlen(buf) - 1] = '\0';
+	
+	/*
+	// enter critical section
+    ret = sem_wait(critical_section);
+    if(ret) {
+        handle_error("sem_wait failed");
+	}
+    */
+    
+	//Critical Section
+	memcpy(users.list[users.online], buf, sizeof(users.list[users.online])); //Copy message recieved to username list
+	memcpy(users.client_ip[users.online], client_ip, sizeof(users.client_ip[users.online])); 
+	users.client_port[users.online] = client_port;
+	id = users.online;
+	users.online++;
+	//Critical Section
+	printOnlineUsers(id);
+	
+	/*
+	// exit critical section
+    ret = sem_post(critical_section);
+	if(ret) {
+	    handle_error("sem_post failed");
+	}
+	*/
+	
+	//WELCOME
+	
+	buf_len = sizeof(buf);
+	memset(buf, 0, buf_len); //TO CHECK 
+    // send custom welcome message - and print user connected on the server thread
     sprintf(buf, "Hi! I'm an echo server. You are %s talking on port %hu.\nI will send you back whatever"
             " you send me. I will stop if you send me %s :-)\n", client_ip, client_port, quit_command);
     msg_len = strlen(buf);
@@ -48,8 +195,9 @@ void connection_handler(int socket_desc, struct sockaddr_in* client_addr) {
         if (ret == -1) handle_error("Cannot write to the socket");
         bytes_sent += ret;
     }
-
-
+	
+	//int bytes_sent = 0;
+	//First value does not arrive
     // echo loop
     while (1) {
         // read message from client
@@ -61,26 +209,102 @@ void connection_handler(int socket_desc, struct sockaddr_in* client_addr) {
             if (ret == -1) handle_error("Cannot read from the socket");
             if (ret == 0) break;
 		} while ( buf[recv_bytes++] != '\n' );
+		
+
+		
         // check whether I have just been told to quit...
         if (recv_bytes == 0) break;
         if (recv_bytes == quit_command_len && !memcmp(buf, quit_command, quit_command_len)) break;
-
+		printf("%s->%s", users.list[id], buf);
         // ... or if I have to send the message back
-        bytes_sent=0;
+        
+        
+        
+        //Try to DELIVER Message to USER if Connected
+        //Searching for User
+        int test = isDeliverable(buf); //if > returns SOCKET Descriptor of Dest, otherwise error
+        //printf("\nResult of test = %d\n",test);
+        
+        if(test > 0){ //MSG is Fine and USER is Online - Sendint to DESTINATARIO
+			bytes_sent=0;
+			while ( bytes_sent < recv_bytes) {
+				ret = send(test, buf + bytes_sent, recv_bytes - bytes_sent, 0);
+				if (ret == -1 && errno == EINTR) continue;
+				if (ret == -1) handle_error("Cannot write to the socket");
+				bytes_sent += ret;
+			}
+			sprintf(buf, "Message Delivered\n");
+		} 
+        else if(test == -3) sprintf(buf, "User not ONLINE\n");
+        else sprintf(buf, "Format of the MESSAGE Incorrect\n");
+    
+		
+        //int return  isDeliverable()
+        //IF Found DELIVERING MESSAGE
+        
+        //IF Not Found write USER NOT CONNECTED
+        
+        //Answer to Sender "DELIVERED or USER NOT CONNECTED"
+        msg_len = strlen(buf);
+		bytes_sent = 0;
+		while ( bytes_sent < msg_len) {
+			ret = send(socket_desc, buf + bytes_sent, msg_len - bytes_sent, 0);
+			if (ret == -1 && errno == EINTR) continue;
+			if (ret == -1) handle_error("Cannot write to the socket");
+			bytes_sent += ret;
+		}
+        
+        
+        //ORIGINAL
+        /*bytes_sent=0;
         while ( bytes_sent < recv_bytes) {
             ret = send(socket_desc, buf + bytes_sent, recv_bytes - bytes_sent, 0);
             if (ret == -1 && errno == EINTR) continue;
             if (ret == -1) handle_error("Cannot write to the socket");
             bytes_sent += ret;
-        }
+        }*/
+        
+        
+        
+        /*//EXPERIMENT
+
+		//Temp Refreshers
+		msg_len = strlen(buf);
+        buf[strlen(buf) - 1] = '\n'; // remove '\n' from the end of the message
+        
+		char temp_buf[1024];
+		memcpy(temp_buf, buf, sizeof(temp_buf));
+		int temp_recv_bytes = recv_bytes;
+		
+		for(int i=0; i<users.online; i++){
+			if(i==id)continue;
+			//Resetting for each user to send
+			memcpy(buf, temp_buf, sizeof(buf));
+			recv_bytes = temp_recv_bytes;
+			bytes_sent=0;
+			
+			while ( bytes_sent < recv_bytes) {
+				ret = send(users.socket_desc[i], buf + bytes_sent, recv_bytes - bytes_sent, 0);
+				if (ret == -1 && errno == EINTR) continue;
+				if (ret == -1) handle_error("Cannot write to the socket");
+				bytes_sent += ret;
+			}
+		}	*/
+        
+        
+        
     }
     // close socket
     ret = close(socket_desc);
+	
+	/*	
+	ret = sem_close(critical_section);
+	if(ret) {
+	    handle_error("sem_close failed");
+    }
+    * */
     if (ret) handle_error("Cannot close socket for incoming connection");
 }
-
-
-
 
 // Wrapper function that take as input handler_args_t struct and then call 
 // connection_handler.
@@ -98,7 +322,8 @@ void *thread_connection_handler(void *arg) {
 
 void mthreadServer(int server_desc) {
     int ret = 0;
-
+	printf("Server ready and waiting to accept connections!\n");
+	
     // loop to manage incoming connections spawning handler threads
     int sockaddr_len = sizeof(struct sockaddr_in);
     while (1) {
@@ -135,6 +360,7 @@ void mthreadServer(int server_desc) {
         handler_args_t *thread_args = malloc(sizeof(handler_args_t));
         thread_args->socket_desc = client_desc;
         thread_args->client_addr = client_addr;
+        users.socket_desc[users.online] = client_desc; //EDIT
         ret = pthread_create(&thread, NULL, thread_connection_handler, (void *)thread_args);
         if (ret) handle_error_en(ret, "Could not create a new thread");
         
@@ -145,11 +371,19 @@ void mthreadServer(int server_desc) {
     }
 }
 
-
-
 int main(int argc, char* argv[]) {
     int ret;
-
+	
+	/*
+	// critical section named semaphore
+	sem_unlink(CRITICAL_SECTION);
+    critical_section = sem_open(CRITICAL_SECTION, O_CREAT|O_EXCL, 0600, 1);
+	if (critical_section == SEM_FAILED) {
+		handle_error("Error creating named semaphore");
+	}
+	*/	
+    
+    
     int socket_desc;
 
     // some fields are required to be filled with 0
@@ -179,9 +413,7 @@ int main(int argc, char* argv[]) {
     ret = listen(socket_desc, MAX_CONN_QUEUE);
     if (ret) handle_error("Cannot listen on socket");
 
-
     mthreadServer(socket_desc);
-
 
     exit(EXIT_SUCCESS); // this will never be executed
 }
